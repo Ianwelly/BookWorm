4bdde7ef7fbd6b2138de65636358eba1a7b8498b
Ian Welsh <derekandclive1@googlemail.com>
2017-07-03 13:52:51 +0100

    Added empty state text view. Added loading indicator. Added message in case of no internet connection.

--- app/src/main/java/com/example/android/bookworm/MainActivity.java
+++ app/src/main/java/com/example/android/bookworm/MainActivity.java
@@ -17,15 +17,18 @@ package com.example.android.bookworm;
 
 import android.app.LoaderManager;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +38,7 @@ BookAdapter mAdapter;
 
     private static final String USGS_REQUEST_URL = "https://www.googleapis.com/books/v1/volumes?q=android&maxResults=10";
     private static final int EARTHQUAKE_LOADER_ID = 1;
+    private TextView mEmptyStateTextView;
 
 //    /** URL for earthquake data from the USGS dataset */
 //    private static final String USGS_REQUEST_URL =
@@ -47,6 +51,9 @@ BookAdapter mAdapter;
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
+
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        earthquakeListView.setEmptyView(mEmptyStateTextView);
 
         // Create a new adapter that takes an empty list of earthquakes as input
         mAdapter = new BookAdapter(this, new ArrayList<Book>());
@@ -74,13 +81,39 @@ BookAdapter mAdapter;
             }
         });
 
-        // Get a reference to the LoaderManager, in order to interact with loaders.
-        LoaderManager loaderManager = getLoaderManager();
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
-        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
-        // because this activity implements the LoaderCallbacks interface).
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
+//        // Get a reference to the LoaderManager, in order to interact with loaders.
+//        LoaderManager loaderManager = getLoaderManager();
+//
+//        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+//        // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+//        // because this activity implements the LoaderCallbacks interface).
+//        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
 
     }
@@ -93,13 +126,19 @@ BookAdapter mAdapter;
 
     @Override
     public void onLoadFinished(Loader<List<Book>> loader, List<Book> earthquakes) {
+
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        mEmptyStateTextView.setText(R.string.no_earthquakes);
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
 
         // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
         // data set. This will trigger the ListView to update.
         if (earthquakes != null && !earthquakes.isEmpty()) {
-            mAdapter.addAll(earthquakes);
+          mAdapter.addAll(earthquakes);
         }
     }
 
@@ -109,57 +148,57 @@ BookAdapter mAdapter;
         mAdapter.clear();
     }
 
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then
-     * update the UI with the list of earthquakes in the response.
-     *
-     * AsyncTask has three generic parameters: the input type, a type used for progress updates, and
-     * an output type. Our task will take a String URL, and return an Earthquake. We won't do
-     * progress updates, so the second generic is just Void.
-     *
-     * We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
-     * The doInBackground() method runs on a background thread, so it can run long-running code
-     * (like network activity), without interfering with the responsiveness of the app.
-     * Then onPostExecute() is passed the result of doInBackground() method, but runs on the
-     * UI thread, so it can use the produced data to update the UI.
-     */
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Book>> {
-
-        /**
-         * This method runs on a background thread and performs the network request.
-         * We should not update the UI from a background thread, so we return a list of
-         * {@link Book}s as the result.
-         */
-        @Override
-        protected List<Book> doInBackground(String... urls) {
-            // Don't perform the request if there are no URLs, or the first URL is null
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-
-            List<Book> result = QueryUtils.fetchEarthquakeData(urls[0]);
-            return result;
-        }
-
-        /**
-         * This method runs on the main UI thread after the background work has been
-         * completed. This method receives as input, the return value from the doInBackground()
-         * method. First we clear out the adapter, to get rid of earthquake data from a previous
-         * query to USGS. Then we update the adapter with the new list of earthquakes,
-         * which will trigger the ListView to re-populate its list items.
-         */
-        @Override
-        protected void onPostExecute(List<Book> data) {
-            // Clear the adapter of previous earthquake data
-            mAdapter.clear();
-
-            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
-            // data set. This will trigger the ListView to update.
-            if (data != null && !data.isEmpty()) {
-                mAdapter.addAll(data);
-            }
-        }
-    }
+//    /**
+//     * {@link AsyncTask} to perform the network request on a background thread, and then
+//     * update the UI with the list of earthquakes in the response.
+//     *
+//     * AsyncTask has three generic parameters: the input type, a type used for progress updates, and
+//     * an output type. Our task will take a String URL, and return an Earthquake. We won't do
+//     * progress updates, so the second generic is just Void.
+//     *
+//     * We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
+//     * The doInBackground() method runs on a background thread, so it can run long-running code
+//     * (like network activity), without interfering with the responsiveness of the app.
+//     * Then onPostExecute() is passed the result of doInBackground() method, but runs on the
+//     * UI thread, so it can use the produced data to update the UI.
+//     */
+//    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Book>> {
+//
+//        /**
+//         * This method runs on a background thread and performs the network request.
+//         * We should not update the UI from a background thread, so we return a list of
+//         * {@link Book}s as the result.
+//         */
+//        @Override
+//        protected List<Book> doInBackground(String... urls) {
+//            // Don't perform the request if there are no URLs, or the first URL is null
+//            if (urls.length < 1 || urls[0] == null) {
+//                return null;
+//            }
+//
+//            List<Book> result = QueryUtils.fetchEarthquakeData(urls[0]);
+//            return result;
+//        }
+//
+//        /**
+//         * This method runs on the main UI thread after the background work has been
+//         * completed. This method receives as input, the return value from the doInBackground()
+//         * method. First we clear out the adapter, to get rid of earthquake data from a previous
+//         * query to USGS. Then we update the adapter with the new list of earthquakes,
+//         * which will trigger the ListView to re-populate its list items.
+//         */
+//        @Override
+//        protected void onPostExecute(List<Book> data) {
+//            // Clear the adapter of previous earthquake data
+//            mAdapter.clear();
+//
+//            // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+//            // data set. This will trigger the ListView to update.
+//            if (data != null && !data.isEmpty()) {
+//                mAdapter.addAll(data);
+//            }
+//        }
+//    }
 }
 
 
